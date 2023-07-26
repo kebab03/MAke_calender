3575367c142d8eb654e2277497926feb76a045bb
kebab03 <68882205+kebab03@users.noreply.github.com>
2023-07-26 02:06:37 +0200

    Add files via upload
    work fine

---
+++ workFinaly/templates/fulname.py
@@ -0,0 +1,15 @@
+'''prmnome=input('dammi il nome di prima')
+secnome=input('dammi il nome di sec')
+ 
+def fulmaew(stname, lastname):
+    fulname=stname+lastname
+    print("comple name :",fulname)
+'''  
+#fulmaew(prmnome, secnome)
+prmnome=input('dammi il nome di prima')
+secnome=input('dammi il nome di sec')
+
+def fulmaew(stname,lastname):
+    fulname=stname+lastname
+    print('comple \"  ricorda che \" viene ignorato name : ',fulname)
+fulmaew(prmnome, secnome)
